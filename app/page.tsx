8c45fc0a2989d4be6082f4408c73f0e6b53d6941
Jose Laurito <jigolaurito93@yahoo.com>
2024-07-18 03:41:01 -0500

    installed shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        click me
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
