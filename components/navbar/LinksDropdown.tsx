6f5c92809871fd712f1802368eb8c0e85ec346b9
Jose Laurito <jigolaurito93@yahoo.com>
2024-07-18 03:45:57 -0500

    created components and subcomponents

---
+++ components/navbar/LinksDropdown.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LinksDropdown = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LinksDropdown
---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavSearch = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavSearch
