b07e447c4566c220200543fc4ff88d8703976f33
Jose Laurito <jigolaurito93@yahoo.com>
2024-07-18 04:12:35 -0500

    added icon logo and nav search bar

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { BsHousesFill } from "react-icons/bs";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div>
-      Logo
-    </div>
-  )
-}
+    <Button size="icon" asChild>
+      <Link href="/">
+        <BsHousesFill className="w-6" h-6 />
+      </Link>
+    </Button>
+  );
+};
 
-export default Logo
+export default Logo;
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { Input } from "../ui/input";
 
 const NavSearch = () => {
   return (
-    <div>
-      NavSearch
-    </div>
-  )
-}
+    <Input
+      type="text"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted"
+    />
+  );
+};
 
-export default NavSearch
+export default NavSearch;
